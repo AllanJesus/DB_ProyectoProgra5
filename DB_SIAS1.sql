57d4a6e82a3cff933327cd2ef02c75dd4c327b1d
Allan <36250060+AllanJesus@users.noreply.github.com>
2020-08-15 23:25:35 -0600

    Actualizacion Base

--- DB_SIAS1.sql
+++ DB_SIAS1.sql
@@ -1,201 +1,216 @@
 create database DB_SIAS 
-Use DB_SIAS 
+use DB_SIAS
 
-Go
-create table Admision
+create table admision
 (
-	id_admision int identity(1,1) not null, 
-	id_persona int,
-	nota float,
-	promedio float,
-	estado varchar(50) 
-);
+	id_admision numeric (8) not null, 
+	id_detalle numeric(8)not null,
+	id_persona numeric(8)not null,
+	nota numeric(8) not null,
+	accionesAfi numeric (8) not null,
+	promedio numeric(8) not null,
+	estado varchar (50) not null
+	primary key (id_admision, id_detalle,id_persona)
+)create index IX_admision 
+on admision(id_admision);
 
-Go
-create table Detalle_Admision
+create table detalle_admision
 (
-	id_detalle int identity(1,1) not null,
-	id_admision int,
-	id_accionafirmativa int,
-	puntosaccion int
-);
+	id_detalle numeric (8) identity not null,
+	id_persona numeric(8)not null,
+	id_accionafirmativa numeric(8)not null,
+	puntosaccion numeric (8)not null,
+	estado bit not null,
+	primary key (id_detalle, id_persona, id_accionafirmativa)
+)create index IX_detalle_admision
+on detalle_admision(id_detalle);
 
-Go
-create table AccionAfirmativa
+create table accionafirmativa
 (
-	id_accionafirmativa int identity(1,1) not null,
-	puntos int,
-	Poblaciones varchar,
-	Documentos varchar,
-	lugar varchar,
-	descripcion varchar (50)not null
-);
+	id_accionafirmativa numeric (8) not null, 
+	descripcion varchar (50)not null,
+	primary key (id_accionafirmativa)
+)create index IX_accionafirmativa
+on tipo_identificacion(id_tipo_identificacion);
 
-Go
-create table Acciones_Detalles_Admision
+create table tipo_identificacion
 (
-	id_detalle int  not null,
-	id_accionafirmativa int not null
-);
+	id_tipo_identificacion numeric (8) not null, 
+	descripcion varchar (50)not null,
+	primary key (id_tipo_identificacion)
+)create index IX_tipo_identificacion 
+on tipo_identificacion(id_tipo_identificacion);
 
-Go
-create table Persona
+create table persona
 (
-	id_persona int not null,
-	nombre varchar(50),
-	apellido1 varchar(50), 
-	apellido2 varchar(50),
-	fecha_nacimiento Date,
-	edad int,
-	correo varchar(50)
-);
+	id_persona numeric(8)not null,
+	id_tipo_identificacion  numeric (8) not null, 
+	nombre varchar(50)not null,
+	apellido1 varchar(50)not null, 
+	apellido2 varchar(50)not null,
+	fecha_nacimiento Date not null,
+	id_telefono	numeric(8)not null,
+	edad numeric(8)not null,
+	dirección varchar(50)not null,
+	correo varchar(50)not nulL
+	primary key (id_persona,id_tipo_identificacion,id_telefono)
+)create index IX_persona
+on persona(id_persona);
 
-Go
-create table Usuario
+create table perfil
+(
+	id_perfil numeric (8)not null,
+	descripcion varchar(50)not null,
+	estado bit not null,
+	primary key (id_perfil)
+
+)create index IX_perfil 
+on perfil(id_perfil);
+
+create table usuario
 (	
-	id_usuario int identity(1,1) not null,
-	id_persona int,
+	id_persona numeric(8) identity(1,1) not null,
 	correo varchar(50)not null,
-	contraseÃ±a varchar(50)not null,
+	contraseña varchar(50)not null,
 	codigo varchar(50)not null,
 	estado bit 
-);
+	primary key (id_persona)
+)create index IX_usuario
+on usuario(id_persona);
 
-Go
-create table Usuario_Perfil
+create table usuario_perfil
 (	
-	id_usuario int not null,
-	id_perfil int not null,
-);
-
-Go
-create table Perfil
-(
-	id_perfil int identity(1,1) not null,
-	descripcion varchar not null,
+	id_usuario_perfil numeric(8)not null,
+	id_persona numeric(8)not null,
+	id_perfil numeric (8)not null,
 	estado bit not null,
-);
+	primary key (id_usuario_perfil,id_persona,id_perfil)
 
-Go
-create table Telefono
+)create index IX_usuario_perfil
+on usuario_perfil(id_usuario_perfil);
+
+create table telefonos
 (
-	id_telefono	int not null,
-	id_Persona int,
-	id_tipo_telefono int,
-	descripcion	varchar(50)
-);
+	id_telefono	numeric(8)not null,
+	descripcion	varchar(50)not null,
+	id_tipo_telefono numeric(8)not null,
+	primary key (id_telefono,id_tipo_telefono)
+)create index IX_telefonos
+on telefonos(id_telefono);
 
-Go
-create table Tipo_Telefonos
+create table tipo_telefonos
 (
-	id_tipo_telefono int not null,
-	descripcion varchar (50)
-);
+	id_tipo_telefono numeric (8)not null,
+	descripcion varchar (50)not null,
+	primary key (id_tipo_telefono)
+)create index IX_tipo_telefonos
+on tipo_telefonos(id_tipo_telefono)
 
-Go
-create table Provincia
+
+
+CREATE TABLE [dbo].[PROVINCIAS](
+	[COD_PROVINCIA] [numeric](1, 0) NOT NULL,
+	[DSC_CORTA_PROVINCIA] [nvarchar](255) NULL,
+	[DSC_PROVINCIA] [nvarchar](255) NULL,
+	[LOG_ACTIVO] [numeric](1, 0) NULL,
+ CONSTRAINT [PK_PROVINCIAS] PRIMARY KEY CLUSTERED 
 (
-	id_prov	 int NOT NULL,
-	descripcion	varchar(50)
-);
+	[COD_PROVINCIA] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 
-Go
-create table Canton
+
+CREATE TABLE [dbo].[CANTONES](
+	[COD_PROVINCIA] [numeric](1, 0) NOT NULL,
+	[COD_CANTON] [numeric](2, 0) NOT NULL,
+	[DSC_CANTON] [nvarchar](255) NULL,
+	[LOG_ACTIVO] [numeric](1, 0) NULL,
+ CONSTRAINT [PK_CANTONES] PRIMARY KEY CLUSTERED 
 (
-	id_prov int NOT NULL,
-	id_can  int NOT NULL,
-	descripcion	varchar(50)
-);
+	[COD_PROVINCIA] ASC,
+	[COD_CANTON] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 
-Go
-create table Distrito
+GO
+
+ALTER TABLE [dbo].[CANTONES]  WITH CHECK ADD  CONSTRAINT [FK_CANTONES_PROVINCIAS] FOREIGN KEY([COD_PROVINCIA])
+REFERENCES [dbo].[PROVINCIAS] ([COD_PROVINCIA])
+GO
+
+ALTER TABLE [dbo].[CANTONES] CHECK CONSTRAINT [FK_CANTONES_PROVINCIAS]
+GO
+
+
+
+CREATE TABLE [dbo].[DISTRITOS](
+	[COD_PROVINCIA] [numeric](1, 0) NOT NULL,
+	[COD_CANTON] [numeric](2, 0) NOT NULL,
+	[COD_DISTRITO] [numeric](3, 0) NOT NULL,
+	[DSC_DISTRITO] [nvarchar](255) NULL,
+	[LOG_ACTIVO] [numeric](1, 0) NULL,
+ CONSTRAINT [PK_DISTRITOS] PRIMARY KEY CLUSTERED 
 (
-	id_prov int NOT NULL,
-	id_can  int NOT NULL,
-	id_dis  int NOT NULL,
-	descripcion	varchar(50)
-);
+	[COD_PROVINCIA] ASC,
+	[COD_CANTON] ASC,
+	[COD_DISTRITO] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 
-Go
-create table Barrio
-(	
-	id_prov int NOT NULL,
-	id_can  int NOT NULL,
-	id_dis  int NOT NULL,
-	id_bar  int NOT NULL,
-	descripcion	varchar(50)
-);
+GO
 
-Go
-create table Otra_senas
+ALTER TABLE [dbo].[DISTRITOS]  WITH CHECK ADD  CONSTRAINT [FK_DISTRITOS_CANTONES] FOREIGN KEY([COD_PROVINCIA], [COD_CANTON])
+REFERENCES [dbo].[CANTONES] ([COD_PROVINCIA], [COD_CANTON])
+GO
+
+ALTER TABLE [dbo].[DISTRITOS] CHECK CONSTRAINT [FK_DISTRITOS_CANTONES]
+GO
+
+CREATE TABLE [dbo].[BARRIO](
+	[COD_PROVINCIA] [numeric](1, 0) NOT NULL,
+	[COD_CANTON] [numeric](2, 0) NOT NULL,
+	[COD_DISTRITO] [numeric](3, 0) NOT NULL,
+	[COD_BARRIO] [numeric](4, 0) NOT NULL,
+	[DSC_BARRIO] [nvarchar](255) NULL,
+	[LOG_ACTIVO] [numeric](1, 0) NULL,
+ CONSTRAINT [PK_BARRIO] PRIMARY KEY CLUSTERED 
 (
-	id_prov int NOT NULL,
-	id_can  int NOT NULL,
-	id_dis  int NOT NULL,
-	id_bar  int NOT NULL,
-	id_otras int NOT NULL,
-	id_persona int,
-	descripcion	varchar(50)
-);
+	[COD_PROVINCIA] ASC,
+	[COD_CANTON] ASC,
+	[COD_DISTRITO] ASC,
+	[COD_BARRIO] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 
-------------------------------Primary key-----------------------------------------------
-Go
-alter table admision add constraint Admision_PK primary key(id_admision)
-Go
-alter table Detalle_Admision add constraint Detalle_Admisionn_PK primary key(id_detalle)
-Go
-alter table AccionAfirmativa add constraint AccionAfirmativa_PK primary key(id_accionafirmativa)
-Go
-alter table Acciones_Detalles_Admision add constraint Acciones_Detalle_sAdmision_PK primary key(id_detalle,id_accionafirmativa)
-Go
-alter table Persona add constraint Persona_PK primary key(id_persona)
-Go
-alter table Usuario add constraint Usuario_PK primary key(id_usuario)
-Go
-alter table Usuario_Perfil add constraint Usuario_Perfil_PK primary key(id_usuario,id_perfil)
-Go
-alter table Perfil add constraint Perfil_PK primary key(id_perfil)
-Go
-alter table Telefono add constraint Telefonos_PK primary key(id_telefono)
-Go
-alter table Tipo_Telefonos add constraint Tipo_Telefonos_PK primary key(id_tipo_telefono)
-Go
-alter table Provincia add constraint Provincia_PK primary key(id_prov)
-Go
-alter table Canton add constraint Canton_PK primary key(id_prov,id_can)
-Go
-alter table Distrito add constraint Distrito_PK primary key(id_prov,id_can,id_dis)
-Go
-alter table Barrio add constraint Barrio_PK primary key (id_prov,id_can,id_dis,id_bar)
-Go
-alter table Otra_senas add constraint Otra_senas_PK primary key (id_prov,id_can,id_dis,id_bar,id_otras)
+GO
 
-------------------------------Foreign key--------------------------------
-Go
-alter table Usuario add constraint Persona_Usuario_FK foreign key (id_persona) references Persona(id_persona)
-Go
-alter table Admision add constraint Persona_Admision_FK foreign key (id_persona) references Persona(id_persona)
-Go
-alter table Canton add constraint Canton_Provincia_FK foreign key (id_prov) references Provincia(id_prov)
-Go
-alter table Distrito add constraint Distrito_Canton_FK foreign key (id_prov,id_can) references Canton(id_prov,id_can)
-Go
-alter table Barrio add constraint Barrio_Distrito_FK foreign key (id_prov,id_can,id_dis) references Distrito(id_prov,id_can,id_dis)
-Go
-alter table Otra_senas add constraint Otra_senas_Barrio_FK foreign key (id_prov,id_can,id_dis,id_bar) references Barrio(id_prov,id_can,id_dis,id_bar)
-Go
-alter table Otra_senas add constraint Otra_senas_Persona_FK foreign key (id_persona) references Persona(id_persona)
-Go
-alter table Telefono add constraint Persona_Telefono_FK foreign key (id_persona) references Persona(id_persona)
-Go
-alter table Usuario_Perfil add constraint Usuario_Perfil_Perfil_FK foreign key (id_perfil) references  Perfil(id_perfil)
-Go
-alter table Usuario_Perfil add constraint Usuario_Perfil_Usuario_FK foreign key (id_usuario) references  Usuario(id_usuario)
-Go
-alter table Telefono add constraint Telefonos_Tipo_Telefonos_FK foreign key (id_tipo_telefono) references Tipo_Telefonos(id_tipo_telefono)
-Go
-alter table Detalle_Admision add constraint Detalle_Admision_Admision_FK foreign key (id_admision) references Admision(id_admision)
-Go
-alter table Acciones_Detalles_Admision add constraint Acciones_Detalles_Admision_Detalle_Admision_FK foreign key (id_detalle) references Detalle_Admision(id_detalle)
-Go
-alter table Acciones_Detalles_Admision add constraint Acciones_Detalles_Admision_AccionAfirmativa_FK foreign key (id_accionafirmativa) references AccionAfirmativa(id_accionafirmativa)
+ALTER TABLE [dbo].[BARRIO]  WITH CHECK ADD  CONSTRAINT [FK_BARRIO_DISTRITOS] FOREIGN KEY([COD_PROVINCIA], [COD_CANTON], [COD_DISTRITO])
+REFERENCES [dbo].[DISTRITOS] ([COD_PROVINCIA], [COD_CANTON], [COD_DISTRITO])
+GO
+
+ALTER TABLE [dbo].[BARRIO] CHECK CONSTRAINT [FK_BARRIO_DISTRITOS]
+GO
+
+
+
+
+alter table usuario_perfil add constraint usuario_perfil_FK01 foreign key (id_persona) references usuario(id_persona)
+alter table usuario_perfil add constraint usuario_perfil_FK02 foreign key (id_perfil) references perfil(id_perfil)
+
+alter table admision add constraint admision_FK01 foreign key (id_persona) references persona(id_persona)
+alter table admision add constraint admision_FK02 foreign key (id_detalle) references detalle_admision(id_detalle)
+
+alter table detalle_admision add constraint detalle_admision_FK01 foreign key (id_persona) references persona(id_persona)
+alter table detalle_admision add constraint detalle_admision_FK02 foreign key (id_accionafirmativa) references accionafirmativa(id_accionafirmativa)
+
+alter table persona add constraint persona_FK01 foreign key (id_tipo_identificacion) references tipo_identificacion(id_tipo_identificacion)
+alter table persona add constraint persona_FK02 foreign key (id_telefono) references telefonos(id_telefono)
+
+alter table telefonos add constraint telefonos_FK01 foreign key (id_tipo_telefono) references tipo_telefonos(id_tipo_telefono) 
+
+
+
+alter table direccion add constraint direccion_FK01 foreign key (id_prov) references provincia(id_prov)
+alter table direccion add constraint direccion_FK02 foreign key (id_can) references canton(id_can)
+alter table direccion add constraint direccion_FK03 foreign key (id_dis) references distrito(id_dis)
+alter table direccion add constraint direccion_FK04 foreign key (id_otras) references otra_señas(id_otras)
